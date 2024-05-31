4965ed7447759e6277f9fb50fbb13dd857f6c60f
Nandini-Maheshwari <nandinim1204@gmail.com>
2024-05-31 15:06:34 +0530

    User logout and auth middleware

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -12,7 +12,7 @@ const generateAccessAndRefreshToken = async(userId) => {
         const refreshToken = user.generateRefreshToken()
 
         user.refreshToken = refreshToken 
-        await user.save({ validateBeforeSave: false })
+        await user.save({ validateBeforeSave: false })  
 
         return { accessToken, refreshToken }
 
@@ -150,10 +150,30 @@ const loginUser = asyncHandler(async (req, res) => {
 }) 
 
 const logoutUser = asyncHandler(async(req, res) => {
-    
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: { refreshToken: undefined }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true 
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User Logged Out successfully"))
 })
 
 export {
     registerUser,
     loginUser,
+    logoutUser
 }   
